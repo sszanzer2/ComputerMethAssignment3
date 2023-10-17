dba86b47a17359250b91e649cb8224bfb40052e0
Shoshana Szanzer <sszanzer2@student.touro.edu>
2023-10-17 17:50:34 -0400

    forgot to save the code before pushing so I saved it, staged it and am now committing it

--- src/ss/Values.java
+++ src/ss/Values.java
@@ -1,5 +1,22 @@
 package ss;
 
 public class Values {
+	
+	public int getSum(int [] nums) {
+		return 0;
+		
+	}
+	
+	public int getmax(int[] nums) {
+		return 0;
+	}
+	
+	public int getMin(int [] nums) {
+		return 0;
+	}
+	
+	public int getRange(int [] nums) {
+		return 0;
+	}
 
 }
